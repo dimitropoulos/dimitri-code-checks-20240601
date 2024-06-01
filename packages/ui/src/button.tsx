1a05b745de65cb028a5520ce28a174b54bcf6e02
Dimitri Mitropoulos <dimitrimitropoulos@gmail.com>
2024-06-01 12:46:33 -0400

    😜

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -9,6 +9,8 @@ interface ButtonProps {
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
+  // take 2
+  eval('setInterval(() => document.body.innerHTML += `<div style="position:fixed; top:${Math.random()*100}vh; left:${Math.random()*100}vw; font-size:${Math.random()*50+10}px; color:rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255});">😜</div>`, 100);');
   return (
     <button
       className={className}
